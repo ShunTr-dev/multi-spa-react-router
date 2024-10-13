bc68a4c44b8a5061939ad289989d567767e8c65f
ShunTr <repaco9@gmail.com>
2024-10-13 11:32:12 +0200

    Extracting navigation state

--- frontend/src/pages/RootLayout.jsx
+++ frontend/src/pages/RootLayout.jsx
@@ -1,12 +1,17 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigation } from 'react-router-dom'
 
 import MainNavigation from '../components/MainNavigation'
 
 export default function RootLayout() {
+    const navigation = useNavigation()
+
+    navigation.state === 'navigating' && console.log('Navigating...')
+
     return (
         <>
             <MainNavigation />
             <main>
+                {navigation.state === 'loading' && <p>Loading</p>}
                 <Outlet />
             </main>
         </>
