a58c67d4429ef08acca310a261ad045125ad425f
ShunTr <repaco9@gmail.com>
2024-10-14 12:16:37 +0200

    Deferring data

--- frontend/src/pages/Events/Events.jsx
+++ frontend/src/pages/Events/Events.jsx
@@ -3,22 +3,31 @@ import EventsList from '../../components/EventsList'
 import { Await, defer, json, useLoaderData } from 'react-router-dom'
 
 function Events() {
-    const events = useLoaderData()
+    const { events } = useLoaderData()
 
     return (
-        <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading</p>}>
+        <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading...</p>}>
             <Await resolve={events}>{(loadedEvents) => <EventsList events={loadedEvents} />}</Await>
         </Suspense>
     )
 }
 
+export default Events
+
 async function loadEvents() {
     const response = await fetch('http://localhost:8080/events')
 
     if (!response.ok) {
-        // throw new Response(JSON.stringify({ message: 'could not fetch events' }, { status: 500 }))
-
-        throw json({ message: 'could not fetch events' }, { status: 500 })
+        // return { isError: true, message: 'Could not fetch events.' };
+        // throw new Response(JSON.stringify({ message: 'Could not fetch events.' }), {
+        //   status: 500,
+        // });
+        throw json(
+            { message: 'Could not fetch events.' },
+            {
+                status: 500,
+            }
+        )
     } else {
         const resData = await response.json()
         return resData.events
@@ -30,5 +39,3 @@ export function loader() {
         events: loadEvents(),
     })
 }
-
-export default Events
