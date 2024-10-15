77e590fcada2900bcdb37d3976717476c3577fa7
ShunTr <repaco9@gmail.com>
2024-10-15 20:49:47 +0200

    route protection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,7 @@ import { action as manipulateEventAction } from './components/EventForm'
 import NewsletterPage, { action as newsletterAction } from './pages/Newsletter'
 import AuthenticationPage, { action as AuthAction } from './pages/Authentication'
 import { action as logoutAction } from './pages/Logout'
-import { tokenLoader } from './util/auth'
+import { checkAuthLoader, tokenLoader } from './util/auth'
 
 const router = createBrowserRouter([
     {
@@ -47,6 +47,7 @@ const router = createBrowserRouter([
                                 path: 'edit',
                                 element: <EditEventPage />,
                                 action: manipulateEventAction,
+                                loader: checkAuthLoader,
                             },
                         ],
                     },
@@ -54,6 +55,7 @@ const router = createBrowserRouter([
                         path: 'new',
                         element: <NewEventPage />,
                         action: manipulateEventAction,
+                        loader: checkAuthLoader,
                     },
                 ],
             },
